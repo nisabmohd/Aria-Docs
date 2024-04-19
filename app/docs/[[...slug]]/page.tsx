fa1ac9bd8f9dc225b1a61a2081ff35e4b0e6482c
nisabmohd <nisabmohd@gmail.com>
2024-04-19 23:26:16 +0530

    added breadcrumb

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -1,3 +1,4 @@
+import DocsBreadcrumb from "@/components/docs-breadcrumb";
 import Pagination from "@/components/pagination";
 import Toc from "@/components/toc";
 import { getMarkdownForSlug } from "@/lib/markdown";
@@ -23,14 +24,18 @@ export default async function DocsPage({
   if (!res) notFound();
   return (
     <div className="flex items-start gap-12 ">
-      <div className="flex-[3] py-10 prose prose-zinc dark:prose-invert prose-code:font-code prose-code:bg-zinc-900 prose-pre:bg-zinc-900 prose-headings:scroll-m-20">
-        <h1>{res.frontmatter.title}</h1>
-        <p className="-mt-5 text-muted-foreground text-lg mb-4">
-          {res.frontmatter.description}
-        </p>
-        {res.content}
-        <Pagination pathname={pathName} />
+      <div className="flex-[3] py-10 ">
+        <DocsBreadcrumb paths={slug} />
+        <div className="prose prose-zinc dark:prose-invert prose-code:font-code prose-code:bg-zinc-900 prose-pre:bg-zinc-900 prose-headings:scroll-m-20">
+          <h1>{res.frontmatter.title}</h1>
+          <p className="-mt-5 text-muted-foreground text-lg mb-4">
+            {res.frontmatter.description}
+          </p>
+          {res.content}
+          <Pagination pathname={pathName} />
+        </div>
       </div>
+
       <Toc path={pathName} />
     </div>
   );
--- components/docs-breadcrumb.tsx
+++ components/docs-breadcrumb.tsx
@@ -1,3 +1,40 @@
-export default function DocsBreadcrumb() {
-  return <div>breadcrumb</div>;
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+export default function DocsBreadcrumb({ paths }: { paths: string[] }) {
+  console.log(paths);
+
+  return (
+    <div className="pb-5">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink>Docs</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>{toTitleCase(paths[0])}</BreadcrumbPage>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>{toTitleCase(paths[1])}</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  );
+}
+
+function toTitleCase(input: string): string {
+  const words = input.split("-");
+  const capitalizedWords = words.map(
+    (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+  );
+  return capitalizedWords.join(" ");
 }
