fc8bf01af45717f76ceac9c1f388b67c73ab0dbf
Mohd. Nisab <nisabmohd@gmail.com>
2024-06-25 14:28:40 +0530

    favicon changed and added deploy and sponsor buttons

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
+import { HeartIcon, TriangleIcon } from "lucide-react";
 
 export default function Footer() {
   return (
-    <footer className="border-t w-full h-16">
-      <div className="container flex items-center justify-center h-full text-muted-foreground text-sm flex-wrap">
-        <p className="text-center">
+    <footer className="border-t w-full sm:h-16">
+      <div className="container flex items-center justify-between sm:gap-0 gap-4 h-full text-muted-foreground text-sm flex-wrap sm:py-0 py-3">
+        <p className="sm:text-center">
           Build by{" "}
           <Link
             className="px-1 underline underline-offset-2"
@@ -22,6 +23,22 @@ export default function Footer() {
           </Link>
           .
         </p>
+        <div className="flex gap-4 items-center">
+          <Link
+            href="https://vercel.com/templates/next.js/documentation-template"
+            className={buttonVariants({ variant: "outline", size: "sm" })}
+          >
+            <TriangleIcon className="h-4 w-4 mr-2 text-primary" />
+            Deploy
+          </Link>
+          <Link
+            href="https://github.com/sponsors/nisabmohd"
+            className={buttonVariants({ variant: "outline", size: "sm" })}
+          >
+            <HeartIcon className="h-4 w-4 mr-2 text-red-500" />
+            Sponsor
+          </Link>
+        </div>
       </div>
     </footer>
   );
