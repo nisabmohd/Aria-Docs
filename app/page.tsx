57841b9304b976b026caf0902e329ab51eafe4a9
Mohd. Nisab <nisabmohd@gmail.com>
2024-06-25 14:43:53 +0530

    hero page changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,26 @@
 import { buttonVariants } from "@/components/ui/button";
 import { page_routes } from "@/lib/routes-config";
-import { TerminalIcon } from "lucide-react";
+import { MoveUpRightIcon, TerminalIcon } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-[88vh] flex-col items-center justify-center text-center px-2 py-8">
-      <p className="mb-5 sm:text-xl">Follow along on Twitter</p>
+    <div className="flex sm:min-h-[91vh] min-h-[88vh] flex-col items-center justify-center text-center px-2 py-8">
+      <Link
+        href="https://github.com/nisabmohd/Documentation-Template"
+        target="_blank"
+        className="mb-5 sm:text-lg flex items-center gap-2 underline underline-offset-4"
+      >
+        Follow along on GitHub{" "}
+        <MoveUpRightIcon className="w-4 h-4 font-extrabold" />
+      </Link>
       <h1 className="text-3xl font-bold mb-4 sm:text-7xl">
         An example app built using Next.js and server components.
       </h1>
       <p className="mb-8 sm:text-xl max-w-[800px] text-muted-foreground">
-        Used by some of the worlds largest companies, This unknown lib enables
-        you to create high-quality web applications with the power of web
-        components.
+        This feature-packed documentation template, built with Next.js, offers a
+        sleek and responsive design, perfect for all your project documentation
+        needs.
       </p>
       <div className="flex flex-row items-center gap-5">
         <Link
@@ -34,7 +41,7 @@ export default function Home() {
         </Link>
       </div>
       <span className="flex flex-row items-center gap-2 text-zinc-400 text-md mt-7 -mb-12 max-[800px]:mb-12">
-        <TerminalIcon className="w-4 h-4" /> ~ npx this-library-name@latest
+        <TerminalIcon className="w-4 h-4 mr-1" /> ~ npx this-library-name@latest
       </span>
     </div>
   );
