4457a0a0aabb9cb583af6ee6e650ddc89b8a479d
Mohd. Nisab <nisabmohd@gmail.com>
2024-06-25 14:36:31 +0530

    some responsiveness issues fixed

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,8 +5,8 @@ import { HeartIcon, TriangleIcon } from "lucide-react";
 export default function Footer() {
   return (
     <footer className="border-t w-full sm:h-16">
-      <div className="container flex items-center justify-between sm:gap-0 gap-4 h-full text-muted-foreground text-sm flex-wrap sm:py-0 py-3">
-        <p className="sm:text-center">
+      <div className="container flex items-center sm:justify-between justify-center sm:gap-0 gap-4 h-full text-muted-foreground text-sm flex-wrap sm:py-0 py-3">
+        <p className="text-center">
           Build by{" "}
           <Link
             className="px-1 underline underline-offset-2"
--- components/pagination.tsx
+++ components/pagination.tsx
@@ -7,17 +7,14 @@ export default function Pagination({ pathname }: { pathname: string }) {
   const res = getPreviousNext(pathname);
 
   return (
-    <div className="flex items-center justify-between py-7">
+    <div className="flex items-center justify-between sm:py-7 py-3">
       <div>
         {res.prev && (
           <Link
-            className={buttonVariants({
-              variant: "link",
-              className: "px-0 no-underline hover:no-underline",
-            })}
+            className="flex items-center gap-2 no-underline text-sm px-1"
             href={`/docs/${res.prev.href}`}
           >
-            <ChevronLeftIcon className="w-[1.1rem] h-[1.1rem] mr-1.5" />
+            <ChevronLeftIcon className="w-[1.1rem] h-[1.1rem]" />
             <p>{res.prev.title}</p>
           </Link>
         )}
@@ -25,14 +22,11 @@ export default function Pagination({ pathname }: { pathname: string }) {
       <div>
         {res.next && (
           <Link
-            className={buttonVariants({
-              variant: "link",
-              className: "px-0 no-underline hover:no-underline",
-            })}
+            className="flex items-center gap-2 no-underline text-sm px-1"
             href={`/docs/${res.next.href}`}
           >
             <p>{res.next.title}</p>
-            <ChevronRightIcon className="w-[1.1rem] h-[1.1rem] ml-1.5" />
+            <ChevronRightIcon className="w-[1.1rem] h-[1.1rem]" />
           </Link>
         )}
       </div>
