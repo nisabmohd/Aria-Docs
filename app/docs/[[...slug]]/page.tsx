ea600ffbc45340e1981bfd8790bb0ac60874f1ef
Mohd. Nisab <nisabmohd@gmail.com>
2024-09-09 00:19:44 +0530

    Update page.tsx

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -15,7 +15,7 @@ export default async function DocsPage({ params: { slug = [] } }: PageProps) {
   if (!res) notFound();
   return (
     <div className="flex items-start gap-12">
-      <div className="flex-[3] pt-10">
+      <div className="flex-[3] py-10">
         <Typography>
           <h1 className="text-3xl -mt-2">{res.frontmatter.title}</h1>
           <p className="-mt-4 text-muted-foreground text-[16.5px]">
