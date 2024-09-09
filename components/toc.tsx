cbc5e901e7b3866d1ed4590270d32aafec62cc9e
Mohd. Nisab <nisabmohd@gmail.com>
2024-09-09 12:03:00 +0530

    Update toc.tsx

--- components/toc.tsx
+++ components/toc.tsx
@@ -9,7 +9,7 @@ export default async function Toc({ path }: { path: string }) {
   return (
     <div className="lg:flex hidden toc flex-[1] min-w-[230px] py-8 sticky top-16 h-[95.95vh]">
       <div className="flex flex-col gap-3 w-full pl-2">
-        <h3 className="font-medium text-sm">On this page</h3>
+        <h3 className="font-semibold text-sm">On this page</h3>
         <ScrollArea className="pb-4 pt-0.5">
           <div className="flex flex-col gap-2.5 text-sm dark:text-neutral-300/85 text-neutral-800 ml-0.5">
             {tocs.map(({ href, level, text }) => (
