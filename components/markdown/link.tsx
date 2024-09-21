1dc6a457f57cefd4cb18d2b05cf4e9b283157c61
Mohd. Nisab <nisabmohd@gmail.com>
2024-09-21 16:33:55 +0530

    cli addition

--- components/markdown/link.tsx
+++
@@ -1,7 +0,0 @@
-import NextLink from "next/link";
-import { ComponentProps } from "react";
-
-export default function Link({ href, ...props }: ComponentProps<"a">) {
-  if (!href) return null;
-  return <NextLink href={href} {...props} />;
-}
