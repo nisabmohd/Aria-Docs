9aabf369f71458bb1033c21231e58be9c9b19194
Mohd. Nisab <nisabmohd@gmail.com>
2024-09-24 22:42:33 +0530

    enhanced error and notfound page

--- app/error.tsx
+++ app/error.tsx
@@ -1,6 +1,7 @@
 "use client"; // Error components must be Client Components
 
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 import { useEffect } from "react";
 
 export default function Error({
@@ -11,24 +12,33 @@ export default function Error({
   reset: () => void;
 }) {
   useEffect(() => {
-    // Log the error to an error reporting service
     console.error(error);
   }, [error]);
 
   return (
-    <div className="min-h-[99vh] px-2 py-8 flex flex-col gap-3 items-start">
-      <div>
-        <h2 className="text-5xl font-bold">Oops!</h2>
-        <p className="text-muted-foreground">Something went wrong!</p>
+    <div className="min-h-[87vh] px-2 sm:py-28 py-36 flex flex-col gap-4 items-center">
+      <div className="text-center flex flex-col items-center justify-center w-fit gap-2">
+        <h2 className="text-7xl font-bold pr-1">Oops!</h2>
+        <p className="text-muted-foreground text-md font-medium">
+          Something went wrong {":`("}
+        </p>
+        <p>
+          We&apos;re sorry, but an error occurred while processing your request.
+        </p>
       </div>
-      <Button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </Button>
+      <div className="flex items-center gap-2">
+        <Button
+          onClick={
+            // Attempt to recover by trying to re-render the segment
+            () => reset()
+          }
+        >
+          Reload page
+        </Button>
+        <Link href="/" className={buttonVariants({})}>
+          Back to homepage
+        </Link>
+      </div>
     </div>
   );
 }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,12 +3,14 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="min-h-[99vh] px-2 py-8 flex flex-col gap-3 items-start">
-      <div>
-        <h2 className="text-5xl font-bold">404</h2>
-        <p className="text-muted-foreground">Page not found</p>
+    <div className="min-h-[87vh] px-2 sm:py-28 py-36 flex flex-col gap-4 items-center">
+      <div className="text-center flex flex-col items-center justify-center w-fit gap-2">
+        <h2 className="text-7xl font-bold pr-1">404</h2>
+        <p className="text-muted-foreground text-md font-medium">
+          Page not found {":("}
+        </p>
+        <p>Oops! The page you&apos;re looking for doesn&apos;t exist.</p>
       </div>
-
       <Link href="/" className={buttonVariants({})}>
         Back to homepage
       </Link>
