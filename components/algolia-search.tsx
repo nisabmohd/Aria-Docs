dcb24b5b2f9f69b2e0ea244772922fb2742ba78c
Mohd. Nisab <nisabmohd@gmail.com>
2025-02-25 18:17:04 +0530

    ctrl k text added customly

--- components/algolia-search.tsx
+++ components/algolia-search.tsx
@@ -10,5 +10,20 @@ type AlgoliaProps = {
 };
 
 export default function AlgoliaSearch(props: AlgoliaProps) {
-  return <DocSearch {...props} />;
+  return (
+    <div className="relative">
+      <div className="absolute right-2 top-[0.4rem] hidden items-center gap-0.5 text-xs font-code sm:flex pointer-events-none">
+        <div className="bg-background/30 border rounded-md py-0.5 px-1 dark:border-neutral-700 border-neutral-300">
+          Ctrl
+        </div>
+        <div className="bg-background/30 border rounded-md py-0.5 px-[0.28rem] dark:border-neutral-700 border-neutral-300">
+          K
+        </div>
+      </div>
+      <DocSearch {...props} maxResultsPerGroup={5} />
+      <div className="absolute top-[0.42rem] left-24 text-muted-foreground font-code -tracking-[0.1rem] text-sm pl-[0.081rem] pointer-events-none">
+        ...
+      </div>
+    </div>
+  );
 }
