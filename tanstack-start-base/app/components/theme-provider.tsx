a4f9c9c61f147ae9129f8be36ca423735cd2cbdb
Mohd. Nisab <nisabmohd@gmail.com>
2025-04-28 22:57:30 +0530

    unused code removed

--- tanstack-start-base/app/components/theme-provider.tsx
+++ tanstack-start-base/app/components/theme-provider.tsx
@@ -1,6 +1,6 @@
 import { setThemeServerFn } from "@/lib/theme";
 import { useRouter } from "@tanstack/react-router";
-import { createContext, PropsWithChildren, use, useState } from "react";
+import { createContext, PropsWithChildren, use } from "react";
 
 export type Theme = "light" | "dark";
 
@@ -10,12 +10,10 @@ type Props = PropsWithChildren<{ theme: Theme }>;
 const ThemeContext = createContext<ThemeContextVal | null>(null);
 
 export function ThemeProvider({ children, theme }: Props) {
-  const [_theme, _setTheme] = useState(theme);
   const router = useRouter();
 
   function setTheme(val: Theme) {
     setThemeServerFn({ data: val });
-    _setTheme(val);
     router.invalidate();
   }
 
